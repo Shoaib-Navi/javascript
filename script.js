163cc03f21c2890c6d9bc4603f76ae4185347502
Shoaib Navi <147228272+Shoaib-Navi@users.noreply.github.com>
2025-04-02 11:28:14 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+// function sayhello()
+// {
+//     let you = prompt("what is your name ?");
+//     console.log("hello", you+ "!");
+// }
+
+
+
+
+// let varContainingFunction= function(){
+//     let varInFunction ="I'm in a function.";
+//     console.log("hi there!",varInFunction);
+// }
+
+// varContainingFunction();
+
+// function tester(para1,para2){
+//     return para1 +""+ para2;
+// }
+
+// const arg1 ="argument 1";
+// const arg2 ="argument 2";
+
+// console.log(tester(arg1,arg2));
+
+// function sum(num1,num2){
+//     return num1 + num2;
+// }
+// console.log(sum(2,4))
+
+function sum(){
+    let num1=parseInt(prompt("enter num1:"));
+    let num2=parseInt(prompt("enter num2:"));
+    
+    console.log(num1 + num2);
+}
+sum();
