97dbf9d2fdfb9e1f0a92a898e8defbbd6e9aa6ff
Shoaib Navi <147228272+Shoaib-Navi@users.noreply.github.com>
2025-04-11 13:01:24 +0530

    recursion,nested function,callbacks

--- script.js
+++ script.js
@@ -186,3 +186,170 @@
 // }
 // doingStuff();
 // console.log(x);
+
+
+//immediately invoked function expression
+//this tell that you don't need to store an anoanymous function into variable ,by this without storing  we can invoke it.
+
+// (function (){
+//     console.log("IIFE");
+// })();
+
+// (()=>{
+//     console.log("IIFE");
+// })();
+
+
+// //Recursive function
+// function getRecursive(nr){
+//     console.log(nr);
+//     getRecursive(--nr);
+// }
+
+// getRecursive(3);
+// //this is infinit recursion
+// //this stop when condition overflow
+
+// function getRecursive(nr){
+//     if(nr > 0){
+//         getRecursive(--nr);
+//     }
+//     console.log(nr);
+// }
+
+// getRecursive(3);
+
+//output:0,0,1,2
+//bcoz 0 come when base condition false then it give 0,and 2nd 0 come by condition
+
+
+// function logRecursive(nr){
+//     console.log("started function:",nr);
+//     if(nr > 0){
+//         logRecursive(nr-1);
+//     }else{
+//         console.log("done with recursion");
+//     }
+//     console.log("ended function:",nr);
+// }
+
+// logRecursive(3);
+
+
+//iteration is better then recursion 
+//the performance of recursion is slightly worse than the performance of regular iteration using a loop .so if this ...
+
+
+
+//nested function
+
+// function doOuterFunctionStuff(nr){
+//     console.log("outer function");
+//     doOuterFunctionStuff(nr);
+//     doInnerFunctionStuff(nr);
+//     function doInnerFunctionStuff(x){
+//         console.log(x+7);
+//         console.log("i can access outer variable:",nr) //something is remain
+//     }
+// }
+
+// doOuterFunctionStuff(3);
+
+
+//when one function1 call and function1 is inside the function2 then it will give error that function reference error 
+
+
+//in one scope you only call only one anonymous function
+
+
+//function callbacks
+//--function passed as an argument
+
+// let functionVariable = function() {
+//     console.log("not so secret though");
+// }
+
+// function doFlexibleStuff(executeStuff){
+//     executeStuff();
+//     console.log("inside doFlexibleFunction");
+// }
+
+// doFlexibleStuff(functionVariable);
+
+//synchronous means blocking code
+
+
+//dynamic pages
+//setTimeout()
+//setTmeInterval()
+
+// let youGotThis = function(){
+//     console.log("keep coding")
+// };
+
+// setTimeout(youGotThis,5000);  //settimeout(function or ... ,time)
+
+//setInterval
+
+// let youGotThis = function(){
+//     console.log("keep coding")
+// };
+
+// setInterval(youGotThis,2000);
+
+
+//practice
+let val=10;
+// function tester(val){
+//     val += 10;
+//     if(val<100){
+//         return tester(val);
+//     }
+//     return val;
+// }
+// console.log(tester(val))
+// console.log(val);
+
+
+//2
+// let testFunction = function(){
+//     console.log("hello")
+// }();
+
+//invoke here directly
+
+
+
+// (function(){
+//     console.log("welcome")
+// })();
+
+//3
+// (function(){
+//     console.log("welcome")
+// })();
+
+// let result =(function(){
+//     let firstName ="laurence";
+//     return firstName;
+// })();
+
+// console.log(result);
+
+// (function(firstName){
+//     console.log("My name is "+ firstName);
+// })("Laurence");
+
+
+//4
+// let test2 =(num) => num+5;
+// console.log(test2(14));
+
+//5
+// var addFive1=function addFive1(num){
+//     return num+2;
+// }
+
+// let addFive2 =(num) => num+2;
+// console.log(addFive1(14));
+
